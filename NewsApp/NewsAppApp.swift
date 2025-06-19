491f452fdc1c732ec09af1c8d04e942fcc2a60f2
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-19 11:10:03 -0600

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Benjamin Rojo on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
