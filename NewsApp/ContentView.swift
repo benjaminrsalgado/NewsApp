d7cc4e2c8ec17210ef0218d46a05f6d2d856b8f9
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-19 13:11:08 -0600

    i practice @Publisehd,@StateObject, for each, class, struct, etc...

--- NewsApp/ContentView.swift
+++ NewsApp/ContentView.swift
@@ -9,13 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.purple
+                .ignoresSafeArea()
+            VStack {
+                FeedNew()
+            }
+        }
+      
+    }
+}
+
+// Esta es la calse principal
+class LikeManager: ObservableObject{
+    @Published var likes: Int = 0
+    
+    func sumar(){
+        likes += 1
+    }
+}
+
+//IDENTIFIABLE representa ca una de las noticias su titulo
+struct News: Identifiable{
+    let id = UUID()
+    var titulo: String
+}
+
+//la vista boton y texto
+struct NewsView: View{
+    var pepe: News
+    @StateObject var likeManager = LikeManager()
+    var body: some View{
+        VStack{
+            Text(pepe.titulo)
+                .font(.title)
+                .foregroundColor(.orange)
+            Text("Esta Publicacion tiene  \(likeManager.likes)")
+            Button(action:{
+                likeManager.sumar()
+            }){Text("❤️ Me gusta \(likeManager.likes) ")
+                    .foregroundColor(.black)
+                    
+                
+            }
         }
         .padding()
+    }
+}
+
+//aqui esta la informacion de cada una de las noticias
+struct FeedNew: View{
+    
+    let noticias = [News(titulo: "SwiftUI 5 ha salido"), News(titulo: "WWDC fue increíble"),News(titulo: "Taylor Swift recupero sus masters") ]
+    var body: some View{
+        ScrollView{
+            VStack{
+                ForEach(noticias){ no in
+                    NewsView(pepe: no)
+                }
+            }
+        }
     }
 }
 
